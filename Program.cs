38b79d29d004555976b87b4312dbf5fce1732d89
iguita <igorsbs27@gmail.com>
2026-03-11 16:05:45 -0300

    ajuste de codigo para executar automaticamente no navegador

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using PokeBlazor.Components;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseUrls("http://localhost:5000");
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-// Serviços do jogo
 builder.Services.AddSingleton<PokeService>();
 builder.Services.AddSingleton<GameState>();
 
@@ -18,5 +20,15 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+// ✅ Abre o navegador automaticamente ao iniciar
+app.Lifetime.ApplicationStarted.Register(() =>
+{
+    Process.Start(new ProcessStartInfo
+    {
+        FileName  = "http://localhost:5000",
+        UseShellExecute = true
+    });
+});
 
 app.Run();
